381399170c4473e29d0e4afe84ae9d9f98ea68d8
Violetta <viol.fedoseenko@yandex.ru>
2022-04-30 22:17:29 +0300

    complete task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let maxDepth = 1;
+    arr.forEach(function(thing) {
+      let depth = 1;
+      if ( typeof thing == 'object') {
+        if ( Array.isArray(thing) ) {
+          const newdepthCalc = new DepthCalculator();
+          depth = depth + newdepthCalc.calculateDepth(thing);
+        }
+      }
+      if ( depth > maxDepth){
+        maxDepth = depth; 
+      }
+    });
+  return maxDepth;
   }
 }
 
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    const point = `( ${value} )`;
+    this.chain.push(point);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    try {
+      return this.chain.join('~~');
+    }
+    finally {
+      this.chain = [];
+    }
   }
 };
 
