3bd9cc3691d9ff4ece87d53b3bc26a43066d7bec
Violetta <viol.fedoseenko@yandex.ru>
2022-05-02 22:37:36 +0300

    task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  // копируем исходный массив, так как его по условию изменять нельзя
+  const newArr = arr.slice();
+  newArr.forEach((element, index) => {
+    switch (element) {
+      case '--discard-next':
+        if (newArr[index + 1]) {
+          newArr.splice([index + 1], 1);
+          newArr.splice(index, 1);
+        } else {
+          newArr.splice(index, 1);
+        }
+        break;
+      case '--discard-prev':
+        if (newArr[index - 1]) {
+          newArr.splice([index - 1], 1);
+          newArr.splice(index, 1);
+
+        } else {
+          newArr.splice(index, 1);
+        }
+        break;
+      case '--double-next':
+        if (newArr[index + 1]) {
+          newArr.splice(index, 1, newArr[index + 1]);
+        }
+        else {
+          newArr.splice(index, 1);
+        }
+        break;
+      case '--double-prev':
+
+        if (newArr[index - 1]) {
+          newArr.splice(index, 1, newArr[index - 1]);
+          console.log(newArr);
+
+        }
+        else {
+          newArr.splice(index, 1);
+        }
+        break;
+    }
+
+  });
+  return newArr;
 }
+
+
 
 module.exports = {
   transform
